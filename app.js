0d76491bffc7167c9e8146842ba0609711607414
Kate <lutkatya@mail.ru>
2026-06-06 10:39:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 // ─────────────────────────────────────────
 // 0. КОНФИГУРАЦИЯ — вставьте ваши данные
 // ─────────────────────────────────────────
-const SUPABASE_URL      = 'https://ohihvtjofkiqlafxthxn.supabase.co/rest/v1/';
+const SUPABASE_URL      = 'https://ohihvtjofkiqlafxthxn.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oaWh2dGpvZmtpcWxhZnh0aHhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3MDA1MDAsImV4cCI6MjA5NjI3NjUwMH0.0sYr7zrGqzU82g6oQ2fZ60w-801w6jDXXMHnzKeKmn8';
 
 const { createClient } = supabase;
